39fb7708888a74c85f4950138cb24fabe2ae4f28
PechPavlo <61708124+PechPavlo@users.noreply.github.com>
2020-05-07 15:27:34 +0300

    Updates

---
+++ precache-manifest.208167bef4c75eb2d77aa33c29e7d96f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e1916bc68f47a80646574e8c0749d14",
+    "url": "/udemy-react/index.html"
+  },
+  {
+    "revision": "dfbfbcb16bc9cc5a6c13",
+    "url": "/udemy-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1b49b9cab7dd7c78b0bb",
+    "url": "/udemy-react/static/js/2.c80fc933.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/udemy-react/static/js/2.c80fc933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfbfbcb16bc9cc5a6c13",
+    "url": "/udemy-react/static/js/main.96d90ac3.chunk.js"
+  },
+  {
+    "revision": "0b50a4ff1dccb9adcefa",
+    "url": "/udemy-react/static/js/runtime-main.5971ebee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/udemy-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
